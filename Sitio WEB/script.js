96cd5f09551bd1058085a590bf322742efc4f738
JuniorProgram9810 <132420442+JuniorProgram9810@users.noreply.github.com>
2025-07-08 12:18:10 -0600

    Delete Sitio WEB directory

--- Sitio WEB/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript para menú móvil
-document.querySelector('.menu-icon').addEventListener('click', () => {
-    const menu = document.querySelector('.menu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-});
